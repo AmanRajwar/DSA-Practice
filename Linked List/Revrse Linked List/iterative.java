c1faafc07f85414725e0e090b0118260cec42d57
AmanRajwar <amanrajwar79@gmail.com>
2023-08-22 12:17:07 +0530

    reverse  linked list

---
+++ Linked List/Revrse Linked List/iterative.java
@@ -0,0 +1,69 @@
+
+/**
+ * 
+ * It is an iterative approach
+ * 
+ * Link- https://shorturl.at/hoC28
+ * 
+ * Time complexity= O(n)
+ * space complexity = O(1)
+ * 
+ */
+
+// Following is the structure of the Singly Linked List.
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class iterative {
+
+    public static LinkedListNode<Integer> reverseLinkedList(LinkedListNode<Integer> head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        LinkedListNode<Integer> newHead = null;
+        while (head != null) {
+            LinkedListNode<Integer> frd = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = frd;
+        }
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        LinkedListNode<Integer> head = new LinkedListNode<>(1);
+        head.next = new LinkedListNode<>(2);
+        head.next.next = new LinkedListNode<>(3);
+        head.next.next.next = new LinkedListNode<>(4);
+        head.next.next.next.next = new LinkedListNode<>(5);
+
+        // Print the original linked list
+        System.out.println("Original Linked List:");
+        printLinkedList(head);
+
+        // Reverse the linked list using the reverseLinkedList function
+        LinkedListNode<Integer> reversedHead = recursive.reverseLinkedList(head);
+
+        // Print the reversed linked list
+        System.out.println("\nReversed Linked List:");
+        printLinkedList(reversedHead);
+    }
+
+    // Helper function to print a linked list
+    public static void printLinkedList(LinkedListNode<Integer> head) {
+        LinkedListNode<Integer> current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
